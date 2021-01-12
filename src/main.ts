8805396afb48259956232a44a19258ea9151c287
liuliu <450441058@qq.com>
2021-01-12 16:45:02 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,6 @@ console.log( process.env.NODE_ENV )
 if ( process.env.NODE_ENV == 'production' ) {
   console.log = () => { }
 }
-if ( process.env.NODE_ENV == 'development' ) {
-  process.env.NODE_ENV_URL = 'http://www.baidu.com'
-}
 
 createApp( App )
   .use( store )
