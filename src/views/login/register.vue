91fa6d628f31255f06e741d2e3893d41a80d0994
liu <450441058@qq.com>
2021-01-20 09:15:16 +0800

    格式样式

---
+++ src/views/login/register.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="register"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent( {
+  name: 'Home',
+
+} )
+</script>
+
+<style lang="less" scoped>
+</style>
